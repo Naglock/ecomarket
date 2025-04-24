b17a3d83ce23aa7d87bbd5e0a5eb8cf369f5e1d3
Naglock <iv.huentemilla@duocuc.cl>
2025-04-24 15:40:35 -0400

    Version Preliminar generada con las dependencias Spring Web,Spring Data JPA,Spring Boot DevTools,Spring Security,Spring Actuator,MySQL Driver y Spring Cloud Config.

---
+++ src/test/java/cl/ecomarket/api/EcoMarketSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ecomarket.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMarketSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
