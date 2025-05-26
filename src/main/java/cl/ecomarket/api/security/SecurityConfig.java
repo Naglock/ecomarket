1a4bcd57ec834839bc8acfa2022a4ae9df24e24c
Naglock <iv.huentemilla@duocuc.cl>
2025-05-26 01:05:20 -0400

    Incorporacion de Jwt, decifra el token generado a partir del microservicio gestion de usuario y permisos. En base a ello, se suma una configuracion de seguridad para validar limitar el acceso segun el rol del usuario que entra. Falta configurar los accesos para cada tipo de usuario.

---
+++ src/main/java/cl/ecomarket/api/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package cl.ecomarket.api.security;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers( "/h2-console/**").permitAll()
+                .requestMatchers("/api/v1/productos/**").hasAnyRole("VENDEDOR", "ADMIN")
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("authorities"); // El nombre del claim en el JWT
+        converter.setAuthorityPrefix("");
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return jwtConverter;
+    }
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        String secret = "UnaClaveSecretaSuperLarga123456!";
+        SecretKey key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(key).build();
+    }
+}
+
